e0bd5cdd5f0430a208db4f3932893ffc7174cf71
shin-fued <wachirawit.yea@student.mahidol.com>
2024-12-20 13:12:32 +0700

    working now but need to figure out how to log it somewhere

--- gps.c
+++ gps.c
@@ -1,5 +1,3 @@
-//gcc gps.c -o gps -lcjson && ./gps && rm gps
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -101,29 +99,42 @@ void write_data(const char* data, const char* hist_path, const char* rt_path) {
 
     char* sensor_id = get_sensor_id();
     if (!sensor_id) {
-        fclose(fp);
+        fclose(fp_hist);
         fclose(fp_rt);
         cJSON_Delete(data_json);
         return;
     }
 
-    int len = snprintf(row_hist, strlen(sensor_id), "%s,", sensor_id);
-    int len_rt = snprintf(row_rt, strlen(sensor_id), "%s,", sensor_id);;
 
-    const cJSON* date_j = cJSON_GetObjectItemCaseSensitive(data_json, "time");
-    if (date_j) {
-        char* unf_date = cJSON_PrintUnformatted(date_j);
-        char* date = reformat_date(unf_date, 0);
-        char* time = reformat_date(unf_date, 1);
-        free(unf_date);
+    int len = strlen(sensor_id);
+    write_row(sensor_id, row_hist, 0, len);
+    write_row(sensor_id, row_rt, 0, len);
+    row_hist[len] = ',';
+    row_rt[len] = ',';
+    len+=1;
+    int len_rt=len;
 
-        if (date && time) {
-            len += snprintf(row_hist, strlen(date), "%s,", date);
-            len_rt += snprintf(row_rt, strlen(time), "%s,", time);
-            free(date);
-            free(time);
-        }
-    }
+    char time_stamp[20];
+    char tm[9];
+    time_t now = time(NULL);
+    struct tm* tm_struct = localtime(&now);
+    strftime(time_stamp, sizeof(time_stamp), "%Y-%m-%d %H:%M:%S", tm_struct);
+    strftime(tm, sizeof(tm), "%H:%M:%S", tm_struct);
+    //printf("time_stamp: %s\n", time_stamp);
+    //printf("time: %s", tm);
+    //free(tm_struct);
+    //write into the rows
+
+    write_row(time_stamp, row_hist, len, len+strlen(time_stamp));
+    len+=strlen(time_stamp);
+    write_row(tm, row_rt, len_rt, len_rt+strlen(tm));
+    len_rt+=strlen(tm);
+    
+
+    row_hist[len] = ',';
+    row_rt[len_rt] = ',';
+    len+=1;
+    len_rt+=1;
 
     const cJSON* lat_j = cJSON_GetObjectItemCaseSensitive(data_json, "lat");
     const cJSON* lon_j = cJSON_GetObjectItemCaseSensitive(data_json, "lon");
@@ -131,24 +142,41 @@ void write_data(const char* data, const char* hist_path, const char* rt_path) {
 
     if (lat_j) {
         char* lat = cJSON_Print(lat_j);
-        len += snprintf(row_hist, strlen(lat), "%s,", lat);
-        len_rt += snprintf(row_rt, strlen(lat), "%s,", lat);
+        write_row(lat, row_hist, len, len+strlen(lat));
+	write_row(lat, row_rt, len_rt, len_rt+strlen(lat));
+	len_rt+=strlen(lat);
+	len+=strlen(lat);
+	row_hist[len] = ',';
+        row_rt[len_rt] = ',';
+        len+=1;
+        len_rt+=1;
         free(lat);
     }
     if (lon_j) {
         char* lon = cJSON_Print(lon_j);
-        len += snprintf(row_hist, strlen(lon), "%s,", lon);
-        len_rt += snprintf(row_rt, strlen(lon), "%s,", lon);
+        write_row(lon, row_hist, len, len+strlen(lon));
+	write_row(lon, row_rt, len_rt, len_rt+strlen(lon));
+	len_rt+=strlen(lon);
+	len+=strlen(lon);
+	row_hist[len] = ',';
+        row_rt[len_rt] = ',';
+        len+=1;
+        len_rt+=1;
         free(lon);
     }
     if (alt_j) {
-        char* alt = cJSON_PrintUnformatted(alt_j);
-        len += snprintf(row_hist, strlen(alt), "%s\n", alt);
-        len_rt += snprintf(row_rt, strlen(alt), "%s\n", alt);
+        char* alt = cJSON_Print(alt_j);
+        write_row(alt, row_hist, len, len+strlen(alt));
+	write_row(alt, row_rt, len_rt, len_rt+strlen(alt));
+	len_rt+=strlen(alt);
+	len+=strlen(alt);
         free(alt);
     }
-    printf("row hist: %s\n", row_hist);
-    printf("row rt: %s\n", row_rt);
+    row_hist[len]='\n';
+    row_hist[len+1]='\0';
+    row_rt[len_rt]='\0';
+    //printf("row hist: %s", row_hist);
+    //printf("row rt: %s\n", row_rt);
 
     fputs(row_hist, fp_hist);
     fputs(row_rt, fp_rt);
@@ -178,24 +206,34 @@ void get_lat_lon_h(const char* hist_path, const char* rt_path) {
 
 int main() {
     char hist_path[MAX_BUFFER_SIZE], rt_path[MAX_BUFFER_SIZE];
-    char date[11];
-    time_t now = time(NULL);
-    struct tm* tm_struct = localtime(&now);
-    strftime(date, sizeof(date), "%Y-%m-%d", tm_struct);
-    char* sensor_id = get_sensor_id();
-    if (!sensor_id){
-	    perror("Error getting sesnor value");
+    
+    char sensor_id[5];
+    char* temp = get_sensor_id();
+    if (!temp){
+	    perror("Error getting sensor value");
 	    return 1;
     }
-    snprintf(hist_path, sizeof(hist_path), "%s%s_gps_%s.csv", GPS_PATH_BASE, sensor_id, date);
+    strcpy(sensor_id, temp);
+    free(temp);
     snprintf(rt_path, sizeof(rt_path), "%s%s_gps_realtime.csv", GPS_PATH_BASE, sensor_id);
-
-    free(sensor_id);
+    const char* csv = ".csv";
+    //printf("rt_path: %s\n", rt_path);
     //printf("rt_path: %s\n", rt_path);
     //printf("hist_path: %s\n", hist_path);
     while (1) {
-       	//get_lat_lon_h(hist_path, rt_path);
-        sleep(10);  // Sleep for 10 seconds
+        char date[11];
+        time_t now = time(NULL);
+        struct tm* tm_struct = localtime(&now);
+        strftime(date, sizeof(date), "%Y-%m-%d", tm_struct);
+        //check if our current day path is complete;
+        if(strstr(hist_path, csv)==NULL){
+            snprintf(hist_path, sizeof(hist_path), "%s%s_gps_%s.csv", GPS_PATH_BASE, sensor_id, date);
+        }
+        
+       	//free(tm_struct);
+        get_lat_lon_h(hist_path, rt_path);
+        sleep(10);
     }
     return 0;
 }
+
